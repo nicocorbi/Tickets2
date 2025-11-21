93ed90fde041725f0dd70d9b82f9589868deaeea
nicocorbi <nicolascorbi2002@gmail.com>
2025-11-21 12:01:11 +0100

    descenso a la locura

--- app/src/main/java/com/example/tickets/ViewModel/TicketViewModel.java
+++ app/src/main/java/com/example/tickets/ViewModel/TicketViewModel.java
@@ -1,17 +1,36 @@
 package com.example.tickets.ViewModel;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.tickets.Model.EstadoTicket;
 import com.example.tickets.Model.Ticket;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TicketViewModel extends ViewModel {
     private final MutableLiveData<List<Ticket>> tickets = new MutableLiveData<>(new ArrayList<>());
     private final MutableLiveData<Ticket> selectedTicket = new MutableLiveData<>();
+
+    private final String File = "tickets.txt";
+
+    public TicketViewModel(@NonNull Application application){
+        super((Closeable) application);
+        loadTickets();
+    }
 
     public LiveData<List<Ticket>> getTickets() {
         return tickets;
@@ -22,6 +41,7 @@ public class TicketViewModel extends ViewModel {
         if (currentTickets != null) {
             currentTickets.add(ticket);
             tickets.setValue(currentTickets);
+            saveTickets();
         }
     }
 
@@ -40,6 +60,47 @@ public class TicketViewModel extends ViewModel {
             tickets.setValue(currentTickets);
         }
     }
+    public void saveTickets(){
+        try{
+            FileOutputStream fos = getApplication().openFileOutput(File,getApplication().MODE_PRIVATE);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
+            List<Ticket> list = tickets.getValue();
+            if (list != null){
+                for(Ticket t : list){
+                    String line = t.getTitulo() + t.getDescripcion() + t.getRecrearBug() + t.getEstado().name();
+
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+            writer.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+    public void loadTickets(){
+        List<Ticket> list = new ArrayList<>();
+        try{
+            FileInputStream fis = getApplication().openFileInput(File);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("/");
+
+                if (parts.length == 10){
+                    Ticket t = new Ticket(parts[0],parts[1],parts[2]);
+                    t.setEstado(EstadoTicket.valueOf(parts[3]));
+                    list.add(t);
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        tickets.setValue(list);
+    }
+
 
     public void selectTicket(Ticket ticket) {
         selectedTicket.setValue(ticket);
